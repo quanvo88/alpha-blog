d8a2f12898b2a9615d024bb53351e62fe01896d7
Quan Vo <qvo@salsify.com>
2020-12-04 00:24:38 -0500

    setup association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 			flash[:notice] = "Article was created successfully!!!"
 			redirect_to @article
@@ -42,12 +43,12 @@ class ArticlesController < ApplicationController
 	private
 
 	def set_article
-		@article = Article.find(params[:id])	
+		@article = Article.find(params[:id])
 	end
 
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
-	
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: { case_sensitivity: false},
   length: { minimum: 3, maximum: 15}
