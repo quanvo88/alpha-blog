d9c2f9fa882924278bb36f9ec6e0e1bc73bdf3a1
Quan Vo <qvo@salsify.com>
2020-12-04 00:01:57 -0500

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, presence: true, uniqueness: { case_sensitivity: false},
+  length: { minimum: 3, maximum: 15}
+  validates :email, presence: true, uniqueness: { case_sensitivity: false},
+  length: { maximum: 200}, format: { with: VALID_EMAIL_REGEX }
 end
