bb7848b259a48b86000be6462cabaa6bcdf0f713
Quan Vo <qvo@salsify.com>
2020-12-04 00:31:43 -0500

    update show ui user and before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: { case_sensitivity: false},
